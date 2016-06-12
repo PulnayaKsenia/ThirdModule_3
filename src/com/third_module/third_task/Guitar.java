67d4f94d357dbe527c9529c424e6899445366afc
PulnayaKsenia <pulnayaksenia@gmail.com>
2016-06-12 17:49:49 +0300

    Initial commit

---
+++ src/com/third_module/third_task/Guitar.java
@@ -0,0 +1,7 @@
+package com.third_module.third_task;
+
+/**
+ * Created by DVPComp on 12.06.2016.
+ */
+public class Guitar extends Instrument {
+}
---
+++ src/com/third_module/third_task/MusicShop.java
@@ -0,0 +1,18 @@
+package com.third_module.third_task;
+
+import java.util.List;
+
+/**
+ * Created by DVPComp on 12.06.2016.
+ */
+public class MusicShop {
+    List<Instrument> instruments;
+
+    public List<Instrument> getInstruments() {
+        return instruments;
+    }
+
+    public void setInstruments(List<Instrument> instruments) {
+        this.instruments = instruments;
+    }
+}
