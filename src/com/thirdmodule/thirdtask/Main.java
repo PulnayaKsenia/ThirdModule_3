4b5f013b40366fb09af9eb6787aa48c4712da8dc
PulnayaKsenia <pulnayaksenia@gmail.com>
2016-07-23 16:04:18 +0300

    exception

--- src/com/thirdmodule/thirdtask/Main.java
+++ src/com/thirdmodule/thirdtask/Main.java
@@ -1,15 +1,16 @@
 package com.thirdmodule.thirdtask;
 
+import java.security.Key;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws WrongKeyException {
         System.out.println("HELLO! This is our music shop!");
 
         Map<String, Integer> shop = new HashMap<>();
         shop.put("guitar", 16);
         shop.put("piano", 2);
-        shop.put("trumpet", 7);
+        shop.put("cello", 0);
         shop.put("violin", 5);
         shop.put("drum", 4);
 
@@ -18,7 +19,7 @@ public class Main {
             System.out.println("Key: " + shopInstrument.getKey() + "; Value: " + shopInstrument.getValue());
 
             if (shopInstrument.getValue().equals(0)) {
-                System.out.println("This item is absent in the stock");
+                System.out.println(shopInstrument.getKey() + " is absent in the stock");
             }
         }
 
@@ -30,19 +31,18 @@ public class Main {
         System.out.println("New order:");
         for (Map.Entry orderNew : order.entrySet()) {
             System.out.println("Key: " + orderNew.getKey() + "; Value: " + orderNew.getValue());
+
+            try {
+                if (!shop.containsKey(orderNew.getKey())) {
+                    throw new WrongKeyException();
+                }
+            } catch (WrongKeyException ex) {
+                System.out.println(orderNew.getKey() + " no such instruments!");
+            }
         }
 
-        try {
-            List<Instrument> remainInstrument = prepareInstrument(shop, order);
-            System.out.println("Remaining instrument in the stock: " + remainInstrument);
-            if (order.keySet() != shop.keySet()) {
-                throw new WrongKeyException();
-            }
-        } catch (IllegalStateException ex) {
-            System.out.println("There are not instruments in the stock!");
-        } catch (WrongKeyException e) {
-            System.out.println("No such instruments!");
-        }
+        List<Instrument> instruments = prepareInstrument(shop, order);
+        System.out.println("Remaining instrument in the stock: " + instruments);
     }
 
     public static List<Instrument> prepareInstrument(Map<String, Integer> shop, Map<String, Integer> order) {
@@ -51,7 +51,7 @@ public class Main {
         int trumpetsToRemove = order.get("trumpet");
 
         if (shop.get("guitar") < guitarsToRemove || shop.get("piano") < pianosToRemove ||
-                shop.get("trumpet") < trumpetsToRemove) throw new IllegalStateException();
+                shop.get("trumpet") < trumpetsToRemove);
 
         List<Instrument> result = new ArrayList<>();
         for (int i = 0; i < pianosToRemove; i++) {
